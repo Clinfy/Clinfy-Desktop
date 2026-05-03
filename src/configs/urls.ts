30199ed0dec786e27b300167acb818dcae36f33d
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2026-05-03 02:32:16 -0300

    Add `urls.ts` for managing authentication-related URLs

---
+++ src/configs/urls.ts
@@ -0,0 +1,8 @@
+const AUTH_URL = 'https://clinfy-auth.aliendo.qzz.io'
+
+export const urls = {
+    auth: {
+        login: `${AUTH_URL}/users/login`,
+        logout: `${AUTH_URL}/users/logout`,
+    }
+}
