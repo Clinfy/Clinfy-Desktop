f1391219fc851e789e8f1af19f29b6bf58239987
Agustin Liendo Ortiz <152560063+AgusLiendo@users.noreply.github.com>
2026-06-01 19:11:25 -0300

    Add refreshToken endpoint to auth URLs in configs

--- src/configs/urls.ts
+++ src/configs/urls.ts
@@ -4,6 +4,7 @@ export const urls = {
     auth: {
         login: `${AUTH_URL}/users/login`,
         logout: `${AUTH_URL}/users/logout`,
+        refreshToken: `${AUTH_URL}/users/refresh-token`,
         sessionContext: `${AUTH_URL}/users/me/session-context`,
     }
 }
